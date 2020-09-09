aaf9426caf56102f32b55f083b8289d7f5306ca0
荆启超 <1209446716@qq.com>
2020-09-09 21:19:44 +0800

    ApplicationContext单例创建对象，BeanFactory多例创建对象

--- snail-spring/spring03_ioc/src/main/java/com/jqc/spring/service/impl/DeptServiceImpl.java
+++ snail-spring/spring03_ioc/src/main/java/com/jqc/spring/service/impl/DeptServiceImpl.java
@@ -7,6 +7,9 @@ import com.jqc.spring.service.DeptService;
 public class DeptServiceImpl implements DeptService {
     private DeptDao deptDao = new DeptDaoImpl();
 
+    public DeptServiceImpl(){
+        System.out.println("对象创建了");
+    }
     public void saveDept() {
         deptDao.saveDept();
     }
